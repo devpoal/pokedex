0c924f6da040df4233f85d3e38e97a22c829fda2
Aleksandr Popovichenko <devpoal@gmail.com>
2020-03-20 23:19:45 +0300

    Updates

---
+++ precache-manifest.f873fc34ee7734f06091d1f1e5608b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ddebd04d8ada3a078f19e4e9bef06a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "7d26c6ae0635d698d290",
+    "url": "/pokedex/static/css/main.60ad07ad.chunk.css"
+  },
+  {
+    "revision": "9b82e31aff29df092345",
+    "url": "/pokedex/static/js/2.f51f4bb8.chunk.js"
+  },
+  {
+    "revision": "647c25ca9e3ea0b71bfc55fa250b5834",
+    "url": "/pokedex/static/js/2.f51f4bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d26c6ae0635d698d290",
+    "url": "/pokedex/static/js/main.ea037db3.chunk.js"
+  },
+  {
+    "revision": "71398026bf69e7d941f1",
+    "url": "/pokedex/static/js/runtime-main.def7b1fa.js"
+  }
+]);
